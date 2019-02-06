83bb1015a42bcf812567190dcbc75583f06fb190
Orlando Correa <orlandocorrea12@yahoo.com>
2019-02-05 20:37:48 -0500

    Finished

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,7 @@
-var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index");
-    });
+  app.get("/", function (req, res) {
+    res.render("index");
   });
 };
